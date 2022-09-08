8a0fa3425998ab4f45c32629c1a45adf1df11481
Bidemsco <Daddyyoyogmail.com>
2022-09-08 13:18:32 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ * * main - Print.c
+ *
+ * *
+ *
+ * * Return: Always 0 (with proper grammar, but the outcome is a piece of art)
+ *
+ * */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual
+
+			puzzle");
+
+	return (0);
+
+}
