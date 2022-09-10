f942526880e54a32aa125ece5707c290a42f4696
Abidemi Osunsanya <111005519+Bidemsco@users.noreply.github.com>
2022-09-10 13:53:16 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
 
-
-
 /**
- *
- * * main - Print.c
- *
- * *
- *
- * * Return: Always 0 (with proper grammar, but the outcome is a piece of art)
- *
- * */
-
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual
-
-			puzzle");
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
