bd34890ec81d411e1cf7c8766fa3cafa66ecead3
Abidemi Osunsanya <111005519+Bidemsco@users.noreply.github.com>
2022-09-12 15:15:14 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,34 +1,32 @@
-#include <stdio.h>
-#include <stdio.h>
-/**
- * program that prints all possible combinations of two two-digit numbers.
- */
-
-int main ()
-{
-	int nl  =  0,   n2;
-	while(nl  <=99)
-	{
-		if (n2  != nl)
-		{
-			putchar((nl / 10) + 48);
-			putchar((nl % 10) + 48);
-			putchar ('   ');
-			putchar( (n2 / 10) + 48);
-			putchar( (nl % 10) + 48);
-
-			 if(nl  != 98  | | n2 != 99)
-			{
-				putchar(' , ');
-				putchar('   ');
-			}
-		}
-		++n2
-	}
-	++nl;
+#include <stdio.h>                                                                                                                    
+#include <stdio.h>                                                                                                                    
+/**                                                                                                                                   
+ * program that prints all possible combinations of two two-digit numbers.                                                            
+ */                                                                                                                                   
+                                                                                                                                      
+int main ()                                                                                                                           
+{                                                                                                                                     
+        int nl  =  0,   n2;                                                                                                           
+        while(nl  <=99)                                                                                                               
+        {                                                                                                                             
+                if (n2  != nl)                                                                                                        
+                {                                                                                                                     
+                        putchar((nl / 10) + 48);                                                                                      
+                        putchar((nl % 10) + 48);                                                                                      
+                        putchar ('   ');                                                                                              
+                        putchar( (n2 / 10) + 48);                                                                                     
+                        putchar( (nl % 10) + 48);                                                                                     
+                                                                                                                                      
+                         if(nl  != 98  | | n2 != 99)                                                                                  
+                        {                                                                                                             
+                                putchar(' , ');                                                                                       
+                                putchar('   ');                                                                                       
+                        }                                                                                                             
+                }                                                                                                                     
+                ++n2                                                                                                                  
+        }                                                                                                                             
+        ++nl;                                                                                                                         
+}                                                                                                                                     
+putchar(' \n');                                                                                                                       
+return 0;                                                                                                                             
 }
-putchar(' \n');
-return 0;
-}
-
-
