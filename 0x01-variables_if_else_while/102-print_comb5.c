4b7711c5cdea38091578af0e75ed25f1d913203e
Abidemi Osunsanya <111005519+Bidemsco@users.noreply.github.com>
2022-09-12 15:21:56 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -27,5 +27,5 @@ int main ()
         ++nl;                                                                                                                         
 }                                                                                                                                     
 putchar(' \n');                                                                                                                       
-return 0;                                                                                                                             
+return 0;
 }
