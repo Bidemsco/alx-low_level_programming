469b6809b1be8beb129ad9e6ecb90ad997afcb60
Abidemi Osunsanya <111005519+Bidemsco@users.noreply.github.com>
2022-09-27 04:57:45 -0700

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
-* _strspn - Gets the length of a prefix substring.
-* @s: The string to be searched.
-* @accept: The prefix to be measured.
-*
-* Return: The number of bytes in s which
-*         consist only of bytes from accept.
-*/
+ * _strspn - Gets the length of a prefix substring.
+ * @s: The string to be searched.
+ * @accept: The prefix to be measured.
+ *
+ * Return: The number of bytes in s which
+ *         consist only of bytes from accept.
+ */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int bytes = 0;
@@ -22,6 +22,7 @@ unsigned int _strspn(char *s, char *accept)
 				bytes++;
 				break;
 			}
+
 			else if (accept[index + 1] == '\0')
 				return (bytes);
 		}
@@ -30,3 +31,4 @@ unsigned int _strspn(char *s, char *accept)
 	}
 
 	return (bytes);
+}
