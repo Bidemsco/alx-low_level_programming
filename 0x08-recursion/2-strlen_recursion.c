1099b2dd238ad2e690810882f70f9a84b6e66a00
Abidemi Osunsanya <111005519+Bidemsco@users.noreply.github.com>
2022-09-28 12:29:32 -0700

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,16 +9,11 @@ int _strlen_recursion(char *s)
 {
 	int len = 0;
 
-	if (*s == '\0')
-	{
-		return (0);
-	}
-	if (*s != '\0')
+	if (*s)
 	{
 		len++;
-		len += _strlen_recursion(++s);
-	
-			return (len);
+		len += _strlen_recursion(s + 1);
 	}
-	return (0);
+
+	return (len);
 }
