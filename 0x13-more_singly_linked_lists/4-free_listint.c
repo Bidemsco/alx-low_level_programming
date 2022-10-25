d6157cff464542cda547a8800919e2924965fd90
Abidemi Osunsanya <111005519+Bidemsco@users.noreply.github.com>
2022-10-25 12:01:02 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,16 +1,13 @@
 #include "lists.h"
 
 /**
- *
- *free_listint - frees a linked list
- *
- *@head: listint_t list to be freed
- *
+ * free_listint - frees a linked list
+ * @head: listint_t list to be freed
  */
 void free_listint(listint_t *head)
 {
 	listint_t *temp;
-	
+
 	while (head)
 	{
 		temp = head->next;
@@ -18,5 +15,3 @@ void free_listint(listint_t *head)
 		head = temp;
 	}
 }
-
-
